80913130cc3460d394c0e5e3404287e014816869
Med1233 <melmeziani-ext@centreon.com>
2026-01-16 13:44:25 +0100

    fix

--- src/php_pages/example.php
+++
@@ -1,9 +0,0 @@
-<section>
-    <h3>Example PHP</h3>
-    <p>
-        Through this associated file, you can create classic PHP pages with all the embeded code that you can need.<br/>
-    </p>
-    <code>
-        <?php phpinfo(); ?>
-    </code>
-</section>
